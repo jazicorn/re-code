1df12bb786f5ecfb60a2f7a253528db4a7f17485
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-03-04 12:41:45 -0500

    created folder for page code editor

---
+++ frontend/src/Editor/index.test.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import Editor from '.';
+
+test('renders homepage', () => {
+  render(<Editor />);
+});
---
+++ frontend/src/Editor/index.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function Editor() {
+  return (
+    <div className="Editor">
+      <h1>Page: Editor</h1>
+    </div>
+  );
+}
+
+export default Editor;
