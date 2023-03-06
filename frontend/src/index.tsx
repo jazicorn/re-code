8a8701fa4a966de1ef749b73de5250887e8a1eea
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-03-05 22:08:49 -0500

    moved css file to css folder

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
-import './index.css';
-import "@fontsource/fira-code"
-import "@fontsource/material-icons-outlined"
+import './css/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
