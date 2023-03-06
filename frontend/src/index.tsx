39872d578c52ee3e70becd387678c82fc52571f7
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-03-05 20:36:34 -0500

    added font

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
+import "@fontsource/fira-code"
 import App from './App';
 
 const root = ReactDOM.createRoot(
