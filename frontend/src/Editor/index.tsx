0621c6a9e2be6b99a9091c23088233416844dca2
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-03-06 18:24:17 -0500

    added code editor

--- frontend/src/Editor/index.tsx
+++ frontend/src/Editor/index.tsx
@@ -1,9 +1,19 @@
-import './index.css';
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
 
 function Editor() {
+  const onChange = React.useCallback((value: any, viewUpdate: any) => {
+    console.log('value:', value);
+  }, []);
   return (
     <div className="Editor">
       <h1>Page: Editor</h1>
+      <CodeMirror
+        value="console.log('hello world!');"
+        height="200px"
+        extensions={[javascript({ jsx: true })]}
+        onChange={onChange} />
     </div>
   );
 }
